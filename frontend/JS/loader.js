d34ee9a874abfbbd11d4e556b1792c4eb794a2e5
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2022-10-19 22:37:51 +0700

    Landing page overhaul

--- frontend/JS/loader.js
+++ frontend/JS/loader.js
@@ -1,6 +1,11 @@
 $(window).on('load', function () {
-    setTimeout(function() {
+    setTimeout(function () {
         //your code to be executed after 1 second
-        $(".loader-wrapper").fadeOut("slow");
+        $("#loader").fadeOut("slow");
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'instant',
+        });
     }, 2000);
 });
--- frontend/JS/timer.js
+++ frontend/JS/timer.js
@@ -1,30 +1,30 @@
 var countDownDate = new Date("Oct 28, 2022 16:00:00").getTime();
 
 var x = setInterval(function () {
-var now = new Date().getTime();
-var distance = countDownDate - now;
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-if(days<10){
-    days = '0'+days;
-}
-if(hours<10){
-    hours = '0'+hours;
-}
-if(minutes<10){
-    minutes = '0'+minutes;
-}
-if(seconds<10){
-    seconds = '0'+seconds;
-}
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (days < 10) {
+        days = '0' + days;
+    }
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
 
-document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("timer1").innerHTML = days + " วัน " + hours + " ชั่วโมง"
+    document.getElementById("timer2").innerHTML = minutes + " นาที " + seconds + " วินาที ";
 
-if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-}
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
 }, 1000);
