d8ea3902bfd1dd3b69695604aa6fa71dffdf6767
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2022-10-19 11:38:53 +0700

    Change timer font and button color

--- frontend/timer.js
+++ frontend/timer.js
@@ -1,27 +1,28 @@
-
-// Set the date we're counting down to
 var countDownDate = new Date("Oct 28, 2022 16:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function () {
-
-// Get today's date and time
 var now = new Date().getTime();
-
-// Find the distance between now and the count down date
 var distance = countDownDate - now;
-
-// Time calculations for days, hours, minutes and seconds
 var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+if(days<10){
+    days = '0'+days;
+}
+if(hours<10){
+    hours = '0'+hours;
+}
+if(minutes<10){
+    minutes = '0'+minutes;
+}
+if(seconds<10){
+    seconds = '0'+seconds;
+}
 
-// Display the result in the element with id="demo"
 document.getElementById("timer").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
 
-// If the count down is finished, write some text
 if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "EXPIRED";
