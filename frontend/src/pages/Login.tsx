1fe2865c7011d0ec0dc8e597129ba929fbd3dd09
Joo <vijak13019@hotmail.com>
2022-10-21 18:59:58 +0700

    refactor(frontend/pages): deleted unused codes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -125,56 +125,6 @@ const Login: React.FC<Props> = () => {
       </div>
     </main>
   )
-  // return (
-  //   <div className="col-md-12">
-  //     <div className="card card-container">
-  //       <Formik
-  //         initialValues={initialValues}
-  //         validationSchema={validationSchema}
-  //         onSubmit={handleLogin}
-  //       >
-  //         <Form>
-  //           <div className="form-group">
-  //             <label htmlFor="username">Username</label>
-  //             <Field name="username" type="text" className="form-control" />
-  //             <ErrorMessage
-  //               name="username"
-  //               component="div"
-  //               className="alert alert-danger"
-  //             />
-  //           </div>
-
-  //           <div className="form-group">
-  //             <label htmlFor="password">Password</label>
-  //             <Field name="password" type="password" className="form-control" />
-  //             <ErrorMessage
-  //               name="password"
-  //               component="div"
-  //               className="alert alert-danger"
-  //             />
-  //           </div>
-
-  //           <div className="form-group">
-  //             <button type="submit" className="btn btn-primary btn-block" disabled={loading}>
-  //               {loading && (
-  //                 <span className="spinner-border spinner-border-sm"></span>
-  //               )}
-  //               <span>Login</span>
-  //             </button>
-  //           </div>
-
-  //           {message && (
-  //             <div className="form-group">
-  //               <div className="alert alert-danger" role="alert">
-  //                 {message}
-  //               </div>
-  //             </div>
-  //           )}
-  //         </Form>
-  //       </Formik>
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default Login
