aadeb12654cf45158cfba0b2e3d6991b7e010080
Joo <vijak13019@hotmail.com>
2022-10-22 00:46:17 +0700

    fix(frontend/src): changed register page into Thinc's

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import MyRoutes from './routes/routes';
 import { Toaster } from 'react-hot-toast';
 import AuthProvider from './Providers/AuthProvider';
