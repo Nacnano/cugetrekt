5257ed355a6da4e1ebe19349f4a78b9b0f2f802d
Chanatip Pattanapen <lovels2906@gmail.com>
2022-10-22 21:32:49 +0700

    improve UI

--- frontend/src/pages/Resignationinfo.tsx
+++ frontend/src/pages/Resignationinfo.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import DropdownList from "react-widgets/DropdownList";
 import "react-widgets/styles.css";
 import Form from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
+import { Button, FloatingLabel } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -23,80 +23,96 @@ const ResignationInfoPage = () => {
     <main style={{ marginTop: "5em" }}>
       <Container>
         <Form onSubmit={handleSubmit(onSubmit)}>
-          <h1 className="heading"> ข้อมูลในใบลาออก </h1>
+          <h1 className="heading d-flex justify-content-center mb-3">
+            ข้อมูลในใบถอนรายวิชา
+          </h1>
           <Container>
-            <Row>
+            <h3 className="mb-3">ข้อมูลส่วนตัว</h3>
+            <Row className="mb-2">
               <Col xs={2}>
-                <Form.Select required>
-                  <option value="">คำนำหน้า</option>
-                  <option value="1">นาย</option>
-                  <option value="2">นาง</option>
-                  <option value="3">นางสาว</option>
-                </Form.Select>
+                <FloatingLabel label="คำนำหน้า">
+                  <Form.Select>
+                    <option>กรุณาเลือกคำนำหน้า</option>
+                    <option value="1">นาย</option>
+                    <option value="2">นาง</option>
+                    <option value="3">นางสาว</option>
+                  </Form.Select>
+                </FloatingLabel>
               </Col>
               <Col>
                 <Form.Group className="mb-3" controlId="formName">
-                  <Form.Control type="Name" placeholder="ชื่อ" required />
+                  <FloatingLabel label="ชื่อ" className="mb-3">
+                    <Form.Control placeholder="ชื่อ" {...register("name")} />
+                  </FloatingLabel>
                 </Form.Group>
               </Col>
               <Col>
                 <Form.Group className="mb-3" controlId="formName">
-                  <Form.Control type="Name" placeholder="นามสกุล" required />
+                  <FloatingLabel label="นามสกุล" className="mb-3">
+                    <Form.Control placeholder="นามสกุล" />
+                  </FloatingLabel>
                 </Form.Group>
               </Col>
             </Row>
           </Container>
           <Container>
-            <Row>
+            <Row className="mb-2">
               <Col>
                 <Form.Group className="mb-3">
-                  {/* input type="text" placeholder="First name" {...register("First name", {required: true, min: 10, maxLength: 9})} */}
-                  <Form.Control
-                    type="text"
-                    minLength={10}
-                    maxLength={10}
-                    required
-                    placeholder="รหัสนิสิต"
-                  />
+                  <FloatingLabel label="รหัสนิสิต">
+                    <Form.Control
+                      type="text"
+                      placeholder="รหัสนิสิต"
+                      minLength={10}
+                      maxLength={10}
+                    />
+                  </FloatingLabel>
                 </Form.Group>
               </Col>
               <Col>
-                <Form.Select required>
-                  <option value="">คณะ</option>
-                  <option value="1">วิศวกรรมศาสตร์</option>
-                  <option value="2">วิทยา</option>
-                  <option value="3">ฟสกด่่ืเฟหด่ิยวฟ</option>
-                </Form.Select>
+                <FloatingLabel label="คณะ">
+                  <Form.Select>
+                    <option value="">กรุณาเลือกคณะที่กำลังศึกษา</option>
+                    <option value="1">วิศวกรรมศาสตร์</option>
+                    <option value="2">วิทยา</option>
+                    <option value="3">ฟสกด่่ืเฟหด่ิยวฟ</option>
+                  </Form.Select>
+                </FloatingLabel>
               </Col>
               <Col>
-                <Form.Select required>
-                  <option value="">สาขา</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </Form.Select>
+                <FloatingLabel label="สาขา">
+                  <Form.Select>
+                    <option>กรุณาเลือกสาขาที่กำลังศึกษา</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </Form.Select>
+                </FloatingLabel>
               </Col>
             </Row>
           </Container>
           <Container>
-            <Row>
+            <Row className="mb-2">
               <Col>
-                <Form.Select required>
-                  <option value="">ระบบการศึกษา</option>
-                  <option value="1">ทวิภาค</option>
-                  <option value="2">ทวิภาค-นานาชาติ</option>
-                  <option value="3">ไตรภาค</option>
-                </Form.Select>
+                <FloatingLabel label="ระบบการศึกษา">
+                  <Form.Select>
+                    <option>กรุณาเลือกระบบการศึกษา</option>
+                    <option value="1">ทวิภาค</option>
+                    <option value="2">ทวิภาค-นานาชาติ</option>
+                    <option value="3">ไตรภาค</option>
+                  </Form.Select>
+                </FloatingLabel>
               </Col>
               <Col>
                 <Form.Group className="mb-3" controlId="Tel">
-                  <Form.Control
-                    type="Tel"
-                    minLength={10}
-                    maxLength={10}
-                    required
-                    placeholder="เบอร์โทรศัพน์"
-                  />
+                  <FloatingLabel label="เบอร์โทรศัพท์">
+                    <Form.Control
+                      type="Tel"
+                      minLength={10}
+                      maxLength={10}
+                      placeholder="เบอร์โทรศัพท์"
+                    />
+                  </FloatingLabel>
                 </Form.Group>
               </Col>
               <Col>
@@ -104,46 +120,58 @@ const ResignationInfoPage = () => {
                   className="mb-3"
                   controlId="exampleForm.ControlInput1"
                 >
-                  <Form.Control
-                    type="email"
-                    required
-                    placeholder="อีเมล์ (name@example.com)"
-                  />
+                  <FloatingLabel label="อีเมล์">
+                    <Form.Control type="email" placeholder="อีเมล์" />
+                  </FloatingLabel>
                 </Form.Group>
               </Col>
             </Row>
           </Container>
+
           <Container>
-            <Row>
+            <h3 className="mb-3">ข้อมูลการศึกษา</h3>
+            <Row className="mb-2">
               <Col>
-                <Form.Select required>
-                  <option value=""> ภาคการศึกษา</option>
-                  <option value="1">ต้น</option>
-                  <option value="2">ปลาย</option>
-                </Form.Select>
+                <Form.Group className="mb-3">
+                  <FloatingLabel label="ภาคการศึกษา">
+                    <Form.Select required>
+                      <option value="">กรุณาเลือกภาคการศึกษาปัจจุบัน</option>
+                      <option value="1">ต้น</option>
+                      <option value="2">ปลาย</option>
+                    </Form.Select>
+                  </FloatingLabel>
+                </Form.Group>
               </Col>
               <Col>
-                <Form.Group className="mb-3" controlId="yrs">
-                  <Form.Control
-                    type="number"
-                    placeholder="ปีการศึกษา"
-                    required
-                  />
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <FloatingLabel label="ปีการศึกษา">
+                    <Form.Control
+                      type="text"
+                      placeholder="ปีการศึกษา"
+                      value="2565"
+                      disabled
+                    />
+                  </FloatingLabel>
                 </Form.Group>
               </Col>
             </Row>
+            <Row>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  <h4 className="mb-2">สาเหตุที่ขอลาออก</h4>
+                </Form.Label>
+                <Form.Control maxLength={200} required as="textarea" rows={2} />
+              </Form.Group>
+            </Row>
+            <div className="d-flex justify-content-end mb-3">
+              <Button type="submit" size="lg">
+                Download PDF
+              </Button>
+            </div>
           </Container>
-          <Row>
-            <Form.Group className="mb-3">
-              <Form.Label>
-                <h4>สาเหตุที่ขอลาออก</h4>
-              </Form.Label>
-              <Form.Control maxLength={200} required as="textarea" rows={2} />
-            </Form.Group>
-          </Row>
-          <Button variant="primary" type="submit">
-            Download PDF
-          </Button>
         </Form>
       </Container>
     </main>
