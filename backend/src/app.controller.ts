144e5d6e12b0d60b43a4f174a0be423a474614fc
Kamanun Maneesri <jomlovejen7956@gmail.com>
2022-10-23 11:34:24 +0700

    added user service (test)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('jom')
+  async createUser( @Query('id') studentId: string, @Query('pwd') password: string ): Promise<UserModel> {
+    return this.userService.createUser( {studentId, password} );
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma.service'
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}
