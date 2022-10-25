49ac55c24e5792fb889983118ea75aec4798d4be
nac <chotpisit080916c@gmail.com>
2022-10-25 19:31:50 +0700

    feat(backend): setup docker and check ip

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,17 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('ip')
+  getIp(): string[] {
+    return this.appService.getIp();
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get(':id')
-  sayJomnoi(@Param('id') id: string): string {
-    return this.appService.sayJomnoi(id);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { networkInterfaces } from 'os';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello JomnoiZ';
   }
+  getIp(): string[] {
+    const nets = networkInterfaces();
+    const results = Object.create(null);
 
-  sayJomnoi(id: string): string {
-    return `I want to say ${id}.`;
+    for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
+        if (net.family === familyV4Value && !net.internal) {
+          if (!results[name]) {
+            results[name] = [];
+          }
+          results[name].push(net.address);
+        }
+      }
+    }
+    return results;
   }
 }
