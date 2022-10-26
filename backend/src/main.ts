f353f8c362c8c7c266a9711e360f6263e2a36b11
nac <chotpisit080916c@gmail.com>
2022-10-27 01:26:26 +0700

    refactor(backend): remove redundants

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,8 +17,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
