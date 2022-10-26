15d308f866d2fea34e4ee61334bc27bacd87c94a
Kamanun Maneesri <jomlovejen7956@gmail.com>
2022-10-27 02:07:20 +0700

    fixed some print problems

--- backend/src/mydocuments/resignation/resignation.service.ts
+++ backend/src/mydocuments/resignation/resignation.service.ts
@@ -21,8 +21,7 @@ export class ResignationService {
   }
 
   updateResignationData(id: number, resignationDto: ResignationDto) {
-    resignationDto['lastEdit'] = moment().
-    format('L') + ' ' + moment().format('LTS');
+    resignationDto['lastEdit'] = moment().format('L') + ' ' + moment().format('LTS');
     return this.prisma.resignation.update({ where: { id: id }, data: resignationDto });
   }
 
@@ -42,8 +41,12 @@ export class ResignationService {
     doc.image('keepimg/resignation.jpg', 0, 0, { width: 595.28 });
     doc.fillColor('#000080');
 
-    doc.text(data['semester'], 270, 152);
-    doc.text(data['year'], 380, 152);
+    if (data['semester'] !== null) {
+      doc.text(data['semester'], 270, 152);
+    }
+    if (data['year'] !== null) {
+      doc.text(data['year'], 380, 152);
+    }
 
     if (data['studySystem'] === 1) {
       doc.text('/', 235.4, 174);
@@ -58,38 +61,58 @@ export class ResignationService {
     doc.text('ศ.ดร.สุพจน์ เตชวรสินสกุล', 108, 217.5);
     doc.text('/', 425, 206.7);
 
-    if (data['title'] == 1) {
-      doc.text('___', 78.5, 252.7);
-      doc.text('______', 94.8, 252.7);
-      doc.text('___', 125, 252.7);
+    if (data['title'] !== null) {
+      if (data['title'] == 1) {
+        doc.text('___', 78.5, 252.7);
+        doc.text('______', 94.8, 252.7);
+        doc.text('___', 125, 252.7);
+      }
+      else if (data['title'] == 2) {
+        doc.text('_____', 100, 260);
+      }
+      else if (data['title'] == 3) {
+        doc.text('___', 124, 260);
+      }
     }
-    else if (data['title'] == 2) {
-      doc.text('_____', 100, 260);
+    if (data['name'] === null) {
+      data['name'] = "";
     }
-    else if (data['title'] == 3) {
-      doc.text('___', 124, 260);
+    if (data['surname'] === null) {
+      data['surname'] = "";
     }
-    doc.text(data['name'] + '  ' + data['surname'], 150, 251.2);
+    doc.text(data['name'] + '  ' + data['surname'], 200, 203.5);
 
-    doc.text(data['studentId'][0], 389, 240);
-    doc.text(data['studentId'][1], 405, 240);
-    doc.text(data['studentId'][2], 421.5, 240);
+    if (data['studentId'] !== null) {
+      doc.text(data['studentId'][0], 389, 240);
+      doc.text(data['studentId'][1], 405, 240);
+      doc.text(data['studentId'][2], 421.5, 240);
 
-    doc.text(data['studentId'][3], 441.5, 240);
-    doc.text(data['studentId'][4], 458, 240);
-    doc.text(data['studentId'][5], 473, 240);
-    doc.text(data['studentId'][6], 488.4, 240);
-    doc.text(data['studentId'][7], 505, 240);
+      doc.text(data['studentId'][3], 441.5, 240);
+      doc.text(data['studentId'][4], 458, 240);
+      doc.text(data['studentId'][5], 473, 240);
+      doc.text(data['studentId'][6], 488.4, 240);
+      doc.text(data['studentId'][7], 505, 240);
 
-    doc.text(data['studentId'][8], 524, 240);
-    doc.text(data['studentId'][9], 538.5, 240);
+      doc.text(data['studentId'][8], 524, 240);
+      doc.text(data['studentId'][9], 538.5, 240);
+    }
 
-    doc.text(data['faculty'], 60, 280.3);
-    doc.text(data['department'], 206, 280.3);
-    doc.text(data['tel'], 355, 280.3);
-    doc.text(data['email'], 453, 280.3, { lineBreak: false });
+    if (data['faculty'] !== null) {
+      doc.text(data['faculty'], 60, 280.3);
+    }
+    if (data['department'] !== null) {
+      doc.text(data['department'], 206, 280.3);
+    }
+    if (data['tel'] !== null) {
+      doc.text(data['tel'], 355, 280.3);
+    }
+    if (data['email'] !== null) {
+      doc.text(data['email'], 453, 280.3, { lineBreak: false });
+    }
 
-    doc.text(data['reason'], 216, 312.5)
+    if (data['reason'] !== null) {
+      doc.text(data['reason'], 216, 312.5);
+    }
 
     const nowDate = Date.now();
     doc.text(moment().format('L').split('/')[1], 425, 406.5);
@@ -97,5 +120,7 @@ export class ResignationService {
     doc.text(moment().format('L').split('/')[2], 494, 406.5);
 
     doc.end();
+    const url = 'localhost:3001/asset/resignation/resignation-' + id + '.pdf';
+    return { "url": `${url}` };
   }
 }
