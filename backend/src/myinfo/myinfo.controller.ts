5bb42c25557b9836de44d0999b41ac1953126f13
Joo <vijak13019@hotmail.com>
2022-10-27 15:38:39 +0700

    Feat(backend/myinfo): added get and update myinfo

--- backend/src/myinfo/myinfo.controller.ts
+++ backend/src/myinfo/myinfo.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { MyInfoService } from './myinfo.service';
 import { MyInfoDto } from './dto/myinfo.dto';
 import { ApiTags, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { MyInfoEntity } from './entities/myinfo.entity';
+import { JwtAuthGuard } from 'src/common/guards/auth.guard';
+import { AuthGuardRequest } from 'src/common/dto/guard.dto';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('myinfo')
 @ApiTags('MyInfo')
 export class MyInfoController {
-  constructor(private readonly myinfoService: MyInfoService) {}
+  constructor(private readonly myinfoService: MyInfoService,
+    private authService: AuthService) {}
 
   @Post()
   @ApiCreatedResponse({ type: MyInfoEntity })
@@ -15,21 +19,36 @@ export class MyInfoController {
     return this.myinfoService.create(myinfoDto);
   }
 
-  @Get(':id')
+  @Get()
   @ApiOkResponse({ type: MyInfoEntity })
-  findOne( @Param('id') id: string) {
-    return this.myinfoService.findOne(+id);
+  @UseGuards(JwtAuthGuard)
+  async findOne(@Req() req: AuthGuardRequest) {
+    const res = await this.authService.getEmail(req);
+    const email = res.email;
+    const user = await this.myinfoService.findOnebyEmail(email);
+    const id = user.id
+    return this.myinfoService.findOne(id);
   }
-
-  @Patch(':id')
+  
+  @Patch()
   @ApiOkResponse({ type: MyInfoEntity })
-  update( @Param('id') id: string, @Body() MyInfoDto: MyInfoDto) {
-    return this.myinfoService.update(+id, MyInfoDto);
+  @UseGuards(JwtAuthGuard)
+  async update(@Body() MyInfoDto: MyInfoDto, @Req() req: AuthGuardRequest) {
+    const res = await this.authService.getEmail(req);
+    const email = res.email;
+    const user = await this.myinfoService.findOnebyEmail(email);
+    const id = user.id
+    return this.myinfoService.update(id, MyInfoDto);
   }
-
-  @Delete(':id')
+  
+  @Delete()
   @ApiOkResponse({ type: MyInfoEntity })
-  remove( @Param('id') id: string) {
-    return this.myinfoService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  async remove(@Req() req: AuthGuardRequest) {
+    const res = await this.authService.getEmail(req);
+    const email = res.email;
+    const user = await this.myinfoService.findOnebyEmail(email);
+    const id = user.id
+    return this.myinfoService.remove(id);
   }
 }
--- backend/src/myinfo/myinfo.module.ts
+++ backend/src/myinfo/myinfo.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { MyInfoService } from './myinfo.service';
 import { MyInfoController } from './myinfo.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthController } from 'src/auth/auth.controller';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [MyInfoController],
-  providers: [MyInfoService],
+  providers: [MyInfoService, AuthController, AuthService, JwtService],
   imports: [PrismaModule],
 })
 export class MyInfoModule {}
