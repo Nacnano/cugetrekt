91dd6f681563908862139a7f34f861b38efbecf7
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2022-10-28 14:34:52 +0700

    Feat: Add AboutUs page placeholder

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { useAuth } from '../Providers/AuthProvider'
 const Footer = () => {
   const { isLoggedIn, logout } = useAuth()
   return (
-    <div className="mt-auto" style={{zIndex: "2000"}}>
+    <div className="mt-auto" style={{ zIndex: "2000" }}>
       <div className="container-fluid bg-sd text-white d-flex align-items-center justify-content-center">
         <footer className="py-3 mt-4">
           <h3 className="text-center">Cu Get Rekt</h3>
@@ -13,6 +13,7 @@ const Footer = () => {
             {isLoggedIn ? (<li className="nav-item"><Link to="/MyDocuments" className="text-white nav-link px-2">กรอกเอกสาร</Link></li>) : (<></>)}
             <li className="nav-item"><a href="/ScorePrediction" className="text-white nav-link px-2">ทำนายคะแนน</a></li>
             <li className="nav-item"><a href="https://chula.wellness.in.th" className="text-white nav-link px-2">สายด่วนสุขภาพจิต</a></li>
+            <li className="nav-item"><Link to="/AboutUs" className="text-white nav-link px-2">เกี่ยวกับเรา</Link></li>
           </ul>
         </footer>
       </div>
---
+++ frontend/src/pages/AboutUs.tsx
@@ -0,0 +1,8 @@
+const AboutUs = () => {
+  return (
+    <main style={{ marginTop: "4em" }}>
+      <h1>About Us</h1>
+    </main>
+  );
+};
+export default AboutUs
