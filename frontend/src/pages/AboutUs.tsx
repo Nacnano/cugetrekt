8c313eb15c02608eee1eb06cc46b975464ac1663
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2022-11-03 16:43:46 +0700

    Feat: About Us page

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -1,8 +1,19 @@
 const AboutUs = () => {
   return (
-    <main style={{ marginTop: "4em" }}>
-      <h1>About Us</h1>
-    </main>
+    <main style={{ marginTop: "5.5em" }}>
+      <div className="container">
+        <div className="row">
+          <div className="col" style={{ textAlign: "justify", textJustify: "inter-word" }}>
+            <h1 className="fw-bold display-5 mb-4">เกี่ยวกับเรา</h1>
+            <p className="fs-4">
+              <span className="text-rgb fw-bold">CU Get Rekt</span> เป็นผลงานของนิสิตจุฬาลงกรณ์มหาวิทยาลัยที่ส่งเข้าประกวดในกิจกรรม&nbsp;
+              <span className="fw-bold color-sd">"Thinc. 10 days in 10 years project"</span> ภายใต้คอนเซปต์ <span className="fw-bold">"ปุ่มถอนอยู่ใกล้เพียงแค่เอื้อมมือ"</span>&nbsp;
+              เพื่ออำนวยความสะดวกให้กับนิสิตที่ต้องการถอนอย่างว่องไว และช่วยนิสิตในการตัดสินใจว่าควรถอนหรือไม่
+            </p>
+          </div>
+        </div>
+      </div>
+    </main >
   );
 };
 export default AboutUs
